1226899d8eb3080d8ec9694ecda1ecb18a29deec
sridhar2903 <saisridhard@gmail.com>
2022-03-23 19:35:20 +0530

    final

--- src/Components/Menubar.js
+++ src/Components/Menubar.js
@@ -1,28 +1,28 @@
 import React from 'react'
-import {Link} from 'react-scroll'
-const Menubar = ({hidemenu}) => {
-  return (
-    <>
-      
-            <div className='menustyle' style={{display: 'flex',flexDirection:'column', alignItems:'center',backgroundColor: 'white',fontFamily: 'Lato'
-            }}>
-           
-          <p style={{paddingTop:'20px'}}><Link activeClass="active" to="home" spy={true} smooth={true} onClick={hidemenu}>Home</Link></p>
-          <hr style={{width:'100px',height:'2px',backgroundColor:'silver'}}/>
-          <p style={{paddingTop:'20px'}}><Link  to="about" spy={true} smooth={true} onClick={hidemenu}>About</Link></p>
-          <hr style={{width:'100px',height:'2px',backgroundColor:'silver'}}/>
-          <p style={{paddingTop:'20px'}}><Link  to="projects" spy={true} smooth={true} onClick={hidemenu}>Projects</Link></p>
-          <hr style={{width:'100px',height:'2px',backgroundColor:'silver'}}/>
-          <p style={{paddingTop:'20px'}}><Link  to="resume" spy={true} smooth={true} onClick={hidemenu}>Resume</Link></p>
-          <hr style={{width:'100px',height:'2px',backgroundColor:'silver'}}/>
-          <p style={{paddingTop:'20px'}}><Link  to="contact" spy={true} smooth={true} onClick={hidemenu}>Contact</Link></p>
-          <hr style={{width:'100px',height:'2px',backgroundColor:'silver'}}/>
-            </div>
-            
-          
+import { Link } from 'react-scroll'
+const Menubar = ({ hidemenu }) => {
+    return (
+        <>
 
-    </>
-  )
+            <div className='menustyle' style={{
+                display: 'flex', flexDirection: 'column', alignItems: 'center', backgroundColor: '#aaa9ad', fontFamily: 'Lato'
+            }}>
+
+                <p style={{ paddingTop: '20px' }}><Link activeClass="active" to="home" spy={true} smooth={true} onClick={hidemenu}>Home</Link></p>
+                <hr style={{ width: '400px', height: '2px', backgroundColor: 'silver' }} />
+                <p style={{ paddingTop: '20px' }}><Link to="about" spy={true} smooth={true} onClick={hidemenu}>About</Link></p>
+                <hr style={{ width: '400px', height: '2px', backgroundColor: 'silver' }} />
+                <p style={{ paddingTop: '20px' }}><Link to="projects" spy={true} smooth={true} onClick={hidemenu}>Projects</Link></p>
+                <hr style={{ width: '400px', height: '2px', backgroundColor: 'silver' }} />
+                <p style={{ paddingTop: '20px' }}><Link to="resume" spy={true} smooth={true} onClick={hidemenu}>Resume</Link></p>
+                <hr style={{ width: '400px', height: '2px', backgroundColor: 'silver' }} />
+                <p style={{ paddingTop: '20px' }}><Link to="contact" spy={true} smooth={true} onClick={hidemenu}>Contact</Link></p>
+            </div>
+
+
+
+        </>
+    )
 }
 
 export default Menubar
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link} from 'react-scroll'
+import { Link } from 'react-scroll'
 import { useState } from 'react'
 import Menubar from './Menubar'
 import './style.css'
@@ -7,38 +7,38 @@ import './style.css'
 
 
 
-function Navbar ()
+function Navbar() {
+  const [menu, setMenu] = useState(false)
 
-   {
-    const [menu,setMenu]=useState(false)
-    
-        return (
-            <>
-               <div style={{position:'sticky',top:'0px'}} >
-            <ul className='ulextra' style={{display: 'flex', listStyle: 'none', justifyContent: 'space-around',backgroundColor: '#FBF3E4',height: '74px',fontFamily: 'Lato'
-            ,position:'relative',bottom:'20px',zIndex:'1000'}}>
-           
-          <li style={{paddingTop:'20px'}}><Link activeClass="active" to="home" spy={true} smooth={true}>Home</Link></li>
-          <li style={{paddingTop:'20px'}}><Link  to="about" spy={true} smooth={true}>About</Link></li>
-          <li style={{paddingTop:'20px'}}><Link  to="projects" spy={true} smooth={true}>Projects</Link></li>
-          
-          <li style={{paddingTop:'20px'}}><Link  to="resume" spy={true} smooth={true}>Resume</Link></li>
-          <li style={{paddingTop:'20px'}}><Link  to="contact" spy={true} smooth={true}>Contact</Link></li>
-          <i class="fa fa-bars" onClick={()=>setMenu(!menu)} ></i>
-            </ul>
-           
-           </div>
+  return (
+    <>
+      <div style={{ position: 'sticky', top: '0px' }} >
+        <ul className='ulextra' style={{
+          display: 'flex', listStyle: 'none', justifyContent: 'space-around', backgroundColor: '#FBF3E4', height: '74px', fontFamily: 'Lato'
+          , position: 'relative', bottom: '20px', zIndex: '1000'
+        }}>
 
-           
-           <div className='setmenu'>
-             {menu&&<Menubar hidemenu={()=>setMenu(!menu)}/>}
-            
-            </div>
-            <br/>
-            <br/>
-            </>
-         
-        )
-    }
+          <li style={{ paddingTop: '20px' }}><Link activeClass="active" to="home" spy={true} smooth={true}>Home</Link></li>
+          <li style={{ paddingTop: '20px' }}><Link to="about" spy={true} smooth={true}>About</Link></li>
+          <li style={{ paddingTop: '20px' }}><Link to="projects" spy={true} smooth={true}>Projects</Link></li>
 
-export default  Navbar
+          <li style={{ paddingTop: '20px' }}><Link to="resume" spy={true} smooth={true}>Resume</Link></li>
+          <li style={{ paddingTop: '20px' }}><Link to="contact" spy={true} smooth={true}>Contact</Link></li>
+          <i class="fa fa-bars" onClick={() => setMenu(!menu)} ></i>
+        </ul>
+
+      </div>
+
+
+      <div className='setmenu'>
+        {menu && <Menubar hidemenu={() => setMenu(!menu)} />}
+
+      </div>
+      <br />
+      <br />
+    </>
+
+  )
+}
+
+export default Navbar

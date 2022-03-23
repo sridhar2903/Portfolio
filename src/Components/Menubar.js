bfa133a1cbd0395c7e0277653941f6dc652ae96d
sridhar2903 <saisridhard@gmail.com>
2022-03-23 19:43:14 +0530

    final

--- src/Components/Menubar.js
+++ src/Components/Menubar.js
@@ -9,13 +9,13 @@ const Menubar = ({ hidemenu }) => {
             }}>
 
                 <p style={{ paddingTop: '20px' }}><Link activeClass="active" to="home" spy={true} smooth={true} onClick={hidemenu}>Home</Link></p>
-                <hr style={{ width: '400px', height: '2px', backgroundColor: 'silver' }} />
+                <hr style={{ width:'195px', height: '2px', backgroundColor: 'silver' }} />
                 <p style={{ paddingTop: '20px' }}><Link to="about" spy={true} smooth={true} onClick={hidemenu}>About</Link></p>
-                <hr style={{ width: '400px', height: '2px', backgroundColor: 'silver' }} />
+                <hr style={{ width:'195px', height: '2px', backgroundColor: 'silver' }} />
                 <p style={{ paddingTop: '20px' }}><Link to="projects" spy={true} smooth={true} onClick={hidemenu}>Projects</Link></p>
-                <hr style={{ width: '400px', height: '2px', backgroundColor: 'silver' }} />
+                <hr style={{ width:'195px', height: '2px', backgroundColor: 'silver' }} />
                 <p style={{ paddingTop: '20px' }}><Link to="resume" spy={true} smooth={true} onClick={hidemenu}>Resume</Link></p>
-                <hr style={{ width: '400px', height: '2px', backgroundColor: 'silver' }} />
+                <hr style={{ width:'195px', height: '2px', backgroundColor: 'silver' }} />
                 <p style={{ paddingTop: '20px' }}><Link to="contact" spy={true} smooth={true} onClick={hidemenu}>Contact</Link></p>
             </div>
 
